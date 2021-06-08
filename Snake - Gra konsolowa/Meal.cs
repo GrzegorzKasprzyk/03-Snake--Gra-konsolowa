7bdbcbffa2b72e97b2a471d12e27de14b1b50fcb
GrzegorzKasprzyk <xankrigor87@gmail.com>
2021-06-08 20:35:29 +0200

    add file

---
+++ Snake - Gra konsolowa/Meal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake___Gra_konsolowa
+{
+    public class Meal
+    {
+        public Meal()
+        {
+            Random rand = new Random();
+
+            var x = rand.Next(1, 20);
+            var y = rand.Next(1, 20);
+            CurrentTarget = new Coordinate(x, y);
+            Draw();
+        }
+
+        public Coordinate CurrentTarget { get; set; }
+        void Draw()
+        {
+            Console.SetCursorPosition(CurrentTarget.X , CurrentTarget.Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("$");
+
+        }
+
+    }
+}
